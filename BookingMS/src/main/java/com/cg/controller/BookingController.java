60cff34e9c87816db1ed8db1b5433391e281f952
Akash985 <akashshyamyadav1997@gmail.com>
2020-06-15 01:42:27 +0530

    V1.0 removed clutter from everywhere

--- BookingMS/src/main/java/com/cg/controller/BookingController.java
+++ BookingMS/src/main/java/com/cg/controller/BookingController.java
@@ -23,32 +23,21 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 public class BookingController {
 	@Autowired
 	BookingService bookingService;
-	
-	
-//	Create
-	//http://localhost:9091/bookingCtrl/create
+
 	@PostMapping(value ="/create",consumes = MediaType.APPLICATION_JSON_VALUE,
 	headers="Accept=application/json",produces=MediaType.APPLICATION_JSON_VALUE)
-	@HystrixCommand(fallbackMethod = "whenBookingIdNotFoundss")
+	@HystrixCommand(fallbackMethod = "whenBookingFailsDueTolostDBConnection")
 	ResponseEntity<Booking> createBooking(@RequestBody Booking booking) throws Exception{
 		booking.setBookingStatus("Accepted");
 		Booking savedBooking=bookingService.addNewBooking(booking);
-		System.out.println(savedBooking+"ok");
 		return 	new ResponseEntity<Booking>(savedBooking, HttpStatus.OK);
 	}
 	
-	public  ResponseEntity<Booking> whenBookingIdNotFoundss(Booking booking)
-	{
-		Booking bookingm = new Booking(0l, null, null, null, null, null, null, null);
-		return new ResponseEntity<Booking>(booking, HttpStatus.NOT_FOUND);
-	}
 	
 	
 //	-------------------------------------------------------------------------------------------------------------
-//	-------------------------------------------------------------------------------------------------------------
-//	Read
-//	get all booking
-	//http://localhost:9091/bookingCtrl/fetchAll
+//	----------------------------------------Fetching---------------------------------------------------------------------
+
 	@GetMapping(value ="/fetchAll",produces=MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<List<Booking>> fetchAllBookings(){
 		List<Booking> bookingList=bookingService.getAllBooking();	
@@ -57,9 +46,6 @@ public class BookingController {
 	
 	
 	
-//###################	
-//	get booking by Id
-	//http://localhost:9091/bookingCtrl/fetch/bokId={bkId}
 	@GetMapping(value ="/fetch/bokId={bkId}",produces=MediaType.APPLICATION_JSON_VALUE)
 	@HystrixCommand(fallbackMethod="whenBookingIdNotFound")
 	ResponseEntity<Booking> fetchBookingByBookingId(@PathVariable("bkId")Long bId) throws BookingIdNotFoundException{
@@ -67,27 +53,18 @@ public class BookingController {
 		return 	new ResponseEntity<Booking>(booking, HttpStatus.OK);
 	}
 	
-//	get booking by busId---%%%%%%%%%%%%%%%
-	//http://localhost:9091/bookingCtrl/fetchBookings/userId={uId}/bookId={bkId}
+	
+	
 	@GetMapping(value ="/fetchBookings/userId={uId}/bookId={bkId}",produces=MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<Booking> fetchBookingsByUserIdAndBookingId(@PathVariable("uId")Long uId,@PathVariable("bkId")Long bkId){
 		Booking booking=bookingService.getAllBookingByUserIdAndBookingId(uId, bkId);
-//		System.out.println(booking);
 		return new ResponseEntity<Booking>(booking, HttpStatus.OK);
 	}
 	
-//	get booking by busId---XXXA
-	//http://localhost:9091/bookingCtrl/fetchBookings/busId={bId}
-	@GetMapping(value ="/fetchBookings/busId={bId}",produces=MediaType.APPLICATION_JSON_VALUE)
-	ResponseEntity<List<Booking>> fetchBookingsByBusId(@PathVariable("bId")Long bId){
-		List<Booking> bookingList=bookingService.getAllBookingByBusId(bId);
-		return new ResponseEntity<List<Booking>>(bookingList, HttpStatus.OK);
-	}
+
+
 	
 	
-	
-//	get booking by user id
-	//http://localhost:9091/bookingCtrl/fetchBookings/userId={uId}
 	@GetMapping(value ="/fetchBookings/userId={uId}")//,produces=MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<List<Booking>> fetchBookingsByUserId(@PathVariable("uId")Long uId){
 		List<Booking> bookingList=bookingService.getAllBookingByUserId(uId);
@@ -95,38 +72,13 @@ public class BookingController {
 	}
 	
 	
-//	-------------------------------------------------------------------------------------------------------------
-//	-------------------------------------------------------------------------------------------------------------
 	
 	
-//	Update
-	//for admin use onlyXXXX
-	//###################	
-	
-	@PutMapping(value ="/updateBooking/bkId={bId}",consumes = MediaType.APPLICATION_JSON_VALUE,
-			headers="Accept=application/json",produces=MediaType.APPLICATION_JSON_VALUE)
-	@HystrixCommand(fallbackMethod="whenBookingIdNotFound")
-	ResponseEntity<Booking> updateBookingdetailsByBookingId(@PathVariable("bId")Long bId,@RequestBody Booking booking) throws BookingIdNotFoundException {
-		Booking updatedBooking =bookingService.updateBookingByBookingId(bId, booking);
-		
-		return new ResponseEntity<Booking>(updatedBooking, HttpStatus.OK);		//can change only busId,BookingId,Amount
-	}
-	
-	
-	
-	//for admin use onlyXXXXX
-	@PutMapping(value ="/updateBookingByBusId/busId= {bId}",consumes = MediaType.APPLICATION_JSON_VALUE,
-			headers="Accept=application/json",produces=MediaType.APPLICATION_JSON_VALUE)
-	ResponseEntity<List<Booking>> updateBookingdetailsByBusId(@PathVariable("bId")Long bId,@RequestBody Booking booking) {
-		List<Booking> bookingList=bookingService.updateBookingByBusId(bId, booking);
-		return new ResponseEntity<List<Booking>>(bookingList, HttpStatus.OK);		//changes bookings related to that bus Id if bus chnages can change only busId,BookingId,Amount
-	}
-	
-	
+
 	
 	
 //	-------------------------------------------------------------------------------------------------------------
-//	-------------------------------------------------------------------------------------------------------------
+//	-------------------------------------------------For cancellation------------------------------------------------------------
 	
 //	For Canceling Using update instead of delete 
 	@PutMapping(value ="/updateBookingToCancel/bkId={btId}",
@@ -147,7 +99,7 @@ public class BookingController {
 	}
 	
 //	-------------------------------------------------------------------------------------------------------------
-//	-------------------------------------------------------------------------------------------------------------
+//	---------------------------------------Fall back Method----------------------------------------------------------------------
 	
 	public  ResponseEntity<Booking> whenBookingIdNotFound(Long bId)
 	{
@@ -155,10 +107,13 @@ public class BookingController {
 		return new ResponseEntity<Booking>(booking, HttpStatus.NOT_FOUND);
 	}
 	
-//	public  ResponseEntity<Booking> whenBookingIdNotFoundForCancel(Long bId,Long)
-//	{
-//		Booking booking = new Booking(0l, null, null, null, null, null, null, null);
-//		return new ResponseEntity<Booking>(booking, HttpStatus.NOT_FOUND);
-//	}
+
+	
+	public  ResponseEntity<Booking> whenBookingFailsDueTolostDBConnection(Booking booking)
+	{
+		Booking bookingm = new Booking(0l, null, null, null, null, null, null, null);
+		return new ResponseEntity<Booking>(booking, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 	
 }
